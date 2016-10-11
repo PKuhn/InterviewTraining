160e45baeca9dd60e685b2ac75c6942c50b49d55
pkuhn <mail@patrick-kuhn.com>
2016-10-11 15:16:05 +0200

    Add cache to spreadsheet

--- misc/spreadsheet.py
+++ misc/spreadsheet.py
@@ -1,3 +1,5 @@
+from collections import defaultdict 
+
 class Position:
     def __init__(self, row, column):
         self.row = row
@@ -7,43 +9,62 @@ class Position:
         if not isinstance(other, self.__class__):
             return NotImplemented
         return self.__dict__ == other.__dict__
+
+    def __str__(self):
+        return str(self._as_pair())
+
+    def __hash__(position):
+        return hash(position._as_pair())
     
     def _as_pair(self):
         return self.row, self.column
 
-    def __hash__(position):
-        return hash(position._as_pair())
 
 class SpreadSheet:
     def __init__(self):
         self.cells = {}
-        self.sums = {}
+        self.target_to_summands = {}
+        self.summand_to_target = defaultdict(set)
+        self.cache = {}
 
     def get(self, position):
         if position in self.cells:
             return self.cells[position]
-        if position in self.sums:
+        if position in self.cache:
+            return self.cache[position]
+        if position in self.target_to_summands:
             try:
-                return sum([self.get(pos) for pos in self.sums[position]])
+                result = sum([self.get(pos) for pos in self.target_to_summands[position]])
+                self.cache[position] = result
+                return result
             except ValueError:
                 raise ValueError('One of the subfields not set')
         raise ValueError('Value is not set set')
 
     def set(self, position, value):
-        if position in self.sums:
+        if position in self.target_to_summands:
             raise ValueError('Sum fields can not be set.')
         self.cells[position] = value
+        self._invalidate_cache(position)
         
     def add_sum(self, first, second, target):
         if not self._is_valid_sum(first, target):
             raise ValueError('Circular sum is invalid')
         if not self._is_valid_sum(second, target):
             raise ValueError('Circular sum is invalid')
-        if target not in self.sums:
-            self.sums[target] = set()
-        self.sums[target].add(first)
-        self.sums[target].add(second)
+        if target not in self.target_to_summands:
+            self.target_to_summands[target] = set()
+        self.target_to_summands[target].add(first)
+        self.target_to_summands[target].add(second)
+        self.summand_to_target[first].add(target)
+        self.summand_to_target[second].add(target)
     
+    def _invalidate_cache(self, position):
+        if position in self.cache:
+            del self.cache[position] 
+        for target in self.summand_to_target[position]:
+            self._invalidate_cache(target)
+
     def _is_valid_sum(self, summand, target):
         seen = set()
         summands = [summand]
@@ -53,6 +74,6 @@ class SpreadSheet:
                 return False
             if not current in seen:
                 seen.add(current)
-                if current in self.sums:
-                    summands.extend(self.sums[current])
+                if current in self.target_to_summands:
+                    summands.extend(self.target_to_summands[current])
         return True
